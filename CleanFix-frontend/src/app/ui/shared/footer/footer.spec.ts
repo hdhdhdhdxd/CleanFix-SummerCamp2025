44260611d3714b9d2d6aad608432430017012404
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-07-28 02:08:00 +0200

    refactor: Elimina puntos y comas innecesarios en los archivos footer.spec.ts y footer.ts

--- CleanFix-frontend/src/app/ui/shared/footer/footer.spec.ts
+++ CleanFix-frontend/src/app/ui/shared/footer/footer.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { Footer } from './footer';
+import { Footer } from './footer'
 
 describe('Footer', () => {
-  let component: Footer;
-  let fixture: ComponentFixture<Footer>;
+  let component: Footer
+  let fixture: ComponentFixture<Footer>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Footer]
-    })
-    .compileComponents();
+      imports: [Footer],
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(Footer);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(Footer)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
--- CleanFix-frontend/src/app/ui/shared/footer/footer.ts
+++ CleanFix-frontend/src/app/ui/shared/footer/footer.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.css'
+  styleUrl: './footer.css',
 })
-export class Footer {
-
-}
+export class Footer {}
