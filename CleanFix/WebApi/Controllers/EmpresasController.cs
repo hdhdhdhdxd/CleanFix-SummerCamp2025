93a3dc6782ed63321039a980250f00bd9ddc2aaf
Achediez <hdhdhdhdxdyt@gmail.com>
2025-07-28 14:11:53 +0200

    Añade proyecto WebApi con endpoints de ejemplo y Swagger
    Se ha incorporado un nuevo proyecto WebApi a la solución CleanFix.sln, configurado para .NET 8.0 y con Swashbuckle.AspNetCore para documentación Swagger. Incluye archivos de configuración (appsettings, launchSettings), un controlador WeatherForecast de ejemplo y su modelo, así como el modelo Empresas y el controlador EmpresasController con un endpoint GET en /api/empresas. Se añade también WebApi.http para pruebas rápidas de la API.

---
+++ CleanFix/WebApi/Controllers/EmpresasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers;
+[Route("api/empresas")]
+[ApiController]
+public class EmpresasController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var list = new List<Empresas>
+        {
+            new Empresas{Id = 1, Nombre = "fontaneriaPaco", Direccion = "CalleDeLosTubos", Telefono= "568394732", Email = "fontaneriaPaco@gmail.com", Tipo ="fontaneria", TiempoTrabajo = new TimeSpan(2, 30, 0), Coste = 50.00M},
+            new Empresas{Id = 2, Nombre = "electricidadAntonio", Direccion = "CalleDeLosCables", Telefono= "937849572", Email = "electricidadAntonio@gmail.com", Tipo ="electricidad", TiempoTrabajo = new TimeSpan(3, 0, 0), Coste = 30.00M}
+        };
+        return Ok(list);
+    }
+}
