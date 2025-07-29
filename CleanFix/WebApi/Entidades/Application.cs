4d184edfc8ff40d86771ffeee285f1bba556471a
Alvaro Gutierre <lanchoalvaro2001@gmail.com>
2025-07-29 14:12:38 +0200

    Añadir propiedad Type a Application y migración EF
    Se ha añadido la propiedad Type de tipo IssueType a la clase Application para representar el tipo de problema asociado. Se ha actualizado el modelo y la base de datos mediante una migración de Entity Framework, añadiendo la columna Type a la tabla Applications. También se han generado los archivos de migración y actualizado el snapshot del modelo.

--- CleanFix/WebApi/Entidades/Application.cs
+++ CleanFix/WebApi/Entidades/Application.cs
@@ -8,4 +8,5 @@ public class Application
     public double Price { get; set; } // Precio de la aplicación    
     public double Duration { get; set; } // Duración de la aplicación en horas
     public string Address { get; set; } // Dirección de la aplicación
+    public IssueType Type { get; set; } // Tipo de problema asociado a la aplicación
 }
