d7dd99b4dc218eb95f2537740f800ebbea4d074c
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-07-29 13:20:56 +0200

    feat: integra el componente Brands en la página de inicio

--- CleanFix-frontend/src/app/ui/pages/home/home.ts
+++ CleanFix-frontend/src/app/ui/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { CardInfo } from './components/card-info/card-info'
 import { Hero } from './components/hero/hero'
+import { Brands } from './components/brands/brands'
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, CardInfo],
+  imports: [Hero, CardInfo, Brands],
   templateUrl: './home.html',
 })
 export class Home {}
