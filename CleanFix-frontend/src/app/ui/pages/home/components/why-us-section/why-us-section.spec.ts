d9b66ca57b2e46fea9a9f2b2fa0c86e00890837c
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-07-30 14:08:16 +0200

    feat: añade el componente 'WhyUsSection' con su plantilla y pruebas

---
+++ CleanFix-frontend/src/app/ui/pages/home/components/why-us-section/why-us-section.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+
+import { WhyUsSection } from './why-us-section'
+
+describe('WhyUsSection', () => {
+  let component: WhyUsSection
+  let fixture: ComponentFixture<WhyUsSection>
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WhyUsSection],
+    }).compileComponents()
+
+    fixture = TestBed.createComponent(WhyUsSection)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+})
---
+++ CleanFix-frontend/src/app/ui/pages/home/components/why-us-section/why-us-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+import { WhyUsCard } from '../why-us-card/why-us-card'
+
+@Component({
+  selector: 'app-why-us-section',
+  imports: [WhyUsCard],
+  templateUrl: './why-us-section.html',
+})
+export class WhyUsSection {}
