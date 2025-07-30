309f8d6ee297c882b5fbbd13e5777d7825544941
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-07-30 23:34:09 +0200

    fix: ajusta la estructura de los componentes HTML y mejora la responsividad en la página de inicio

--- CleanFix-frontend/src/app/ui/pages/home/home.ts
+++ CleanFix-frontend/src/app/ui/pages/home/home.ts
@@ -4,10 +4,11 @@ import { Hero } from './components/hero/hero'
 import { Brands } from './components/brands/brands'
 import { ServiceSection } from './components/service-section/service-section'
 import { WhyUsSection } from './components/why-us-section/why-us-section'
+import { Header } from '@/ui/shared/header/header'
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, CardInfo, Brands, ServiceSection, WhyUsSection],
+  imports: [Hero, CardInfo, Brands, ServiceSection, WhyUsSection, Header],
   templateUrl: './home.html',
 })
 export class Home {}
