0d4708b4d2e3ae90d8e37d0c790c7abc1d4c61b8
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-08-01 00:20:50 +0200

    feat: añade el componente de carrusel en LocationsSection y mejora el diseño de las secciones

--- CleanFix-frontend/src/app/ui/pages/home/components/locations-section/locations-section.ts
+++ CleanFix-frontend/src/app/ui/pages/home/components/locations-section/locations-section.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, ElementRef, ViewChild } from '@angular/core'
 import { ImageCard } from '../image-card/image-card'
 
 @Component({
@@ -6,4 +6,23 @@ import { ImageCard } from '../image-card/image-card'
   imports: [ImageCard],
   templateUrl: './locations-section.html',
 })
-export class LocationsSection {}
+export class LocationsSection {
+  @ViewChild('carousel') carousel!: ElementRef
+
+  scrollCarousel(direction: 'left' | 'right') {
+    const scrollAmount = 300 // Ajusta según el tamaño de tus cards
+    const currentScroll = this.carousel.nativeElement.scrollLeft
+
+    if (direction === 'left') {
+      this.carousel.nativeElement.scrollTo({
+        left: currentScroll - scrollAmount,
+        behavior: 'smooth',
+      })
+    } else {
+      this.carousel.nativeElement.scrollTo({
+        left: currentScroll + scrollAmount,
+        behavior: 'smooth',
+      })
+    }
+  }
+}
