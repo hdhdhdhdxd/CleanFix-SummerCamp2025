03a2d5d1d86e7af4520cf2b4783076d0e8806e2e
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-07-31 13:23:31 +0200

    feat: elimina el componente ImageCard de la importación en la página de inicio

--- CleanFix-frontend/src/app/ui/pages/home/home.ts
+++ CleanFix-frontend/src/app/ui/pages/home/home.ts
@@ -4,11 +4,10 @@ import { Hero } from './components/hero/hero'
 import { Brands } from './components/brands/brands'
 import { ServiceSection } from './components/service-section/service-section'
 import { WhyUsSection } from './components/why-us-section/why-us-section'
-import { ImageCard } from './components/image-card/image-card'
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, CardInfo, Brands, ServiceSection, WhyUsSection, ImageCard],
+  imports: [Hero, CardInfo, Brands, ServiceSection, WhyUsSection],
   templateUrl: './home.html',
 })
 export class Home {}
