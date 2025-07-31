1773a1523afeed0d15d1ed2e8fd22e145ad4562e
AritzSanz <aritz.s@hotmail.es>
2025-07-31 14:01:57 +0200

    feat: añade el componente LocationsSection con su plantilla y pruebas

---
+++ CleanFix-frontend/src/app/ui/pages/home/components/locations-section/locations-section.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-locations-section',
+  imports: [],
+  templateUrl: './locations-section.html',
+})
+export class LocationsSection {}
