191ccce686b7e5ff084349d3e16573fcae416d47
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-08-05 01:29:36 +0200

    fix: reemplaza sección de estadísticas por componente "app-stats-section"

--- CleanFix-frontend/src/app/ui/pages/home/home.ts
+++ CleanFix-frontend/src/app/ui/pages/home/home.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core'
-import { CardInfo } from './components/card-info/card-info'
 import { Hero } from './components/hero/hero'
 import { Brands } from './components/brands/brands'
 import { ServiceSection } from './components/service-section/service-section'
 import { WhyUsSection } from './components/why-us-section/why-us-section'
 import { LocationsSection } from './components/locations-section/locations-section'
+import { StatsSection } from './components/stats-section/stats-section'
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, CardInfo, Brands, ServiceSection, WhyUsSection, LocationsSection],
+  imports: [Hero, Brands, ServiceSection, WhyUsSection, LocationsSection, StatsSection],
   templateUrl: './home.html',
 })
 export class Home {}
