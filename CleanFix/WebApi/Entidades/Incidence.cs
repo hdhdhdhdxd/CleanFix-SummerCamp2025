fa39ca5ba3b7be474cb3519b0129c6cd1aabe88d
Alvaro Gutierre <lanchoalvaro2001@gmail.com>
2025-08-05 13:48:23 +0200

    Añade entidades Incidence y Request al proyecto
    Se agregan los archivos Incidence.cs y Request.cs con las clases Incidence y Request para modelar incidencias y solicitudes de mantenimiento. Incidence incluye propiedades como tipo, fecha, estado, descripción, apartamento y prioridad (con enum Priority). Request gestiona solicitudes con descripción, fecha, dirección, estado, costo de mantenimiento y tipo de incidencia. Estas entidades mejoran la gestión y seguimiento de servicios y problemas reportados en la aplicación.

---
+++ CleanFix/WebApi/Entidades/Incidence.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Identity.Client;
+
+namespace WebApi.Entidades;
+
+public class Incidence
+{
+    public Guid Id { get; set; } // Identificador único de la incidencia
+    public IssueType Type { get; set; } // Tipo de incidencia (e.g., "Electricidad", "Fontanería", etc.)
+    public DateTime Date { get; set; } // Fecha de la incidencia
+    public string Status { get; set; } // Estado de la incidencia (e.g., "Pendiente", "En Progreso", "Resuelta")
+    public string Description { get; set; } // Descripción de la incidencia
+    public Apartment? Apartment { get; set; } // Apartamento asociado a la incidencia
+    public Priority Priority { get; set; } // Prioridad de la incidencia (e.g., "Baja", "Media", "Alta", "Crítica")
+}
+
+public enum Priority
+{
+
+   Low,      // Baja prioridad
+   Medium,   // Prioridad media
+   High,     // Alta prioridad
+   Critical  // Prioridad crítica
+}
