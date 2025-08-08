20e345ade244b780362277ba44e548e973b6dfd8
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-08-08 14:25:14 +0200

    Agregar inicialización de repositorios en el punto de entrada de la aplicación

--- CleanFix-frontend/src/app/ui/services/building/building-service.ts
+++ CleanFix-frontend/src/app/ui/services/building/building-service.ts
@@ -1,15 +1,10 @@
 import { buildingService } from '@/core/application/buildingService'
-import { buildingApiRepository } from '@/core/infrastructure/repositories/buildingApiRepository'
 import { Injectable } from '@angular/core'
 
 @Injectable({
   providedIn: 'root',
 })
 export class BuildingService {
-  constructor() {
-    buildingService.init(buildingApiRepository)
-  }
-
   getAll() {
     return buildingService.getAll()
   }
--- CleanFix-frontend/src/main.ts
+++ CleanFix-frontend/src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { appConfig } from './app/config/app.config'
 import { Main } from './app/ui/main/main'
+import { initializeRepositories } from './app/config/repository-init'
+
+initializeRepositories()
 
 bootstrapApplication(Main, appConfig).catch((err) => console.error(err))
