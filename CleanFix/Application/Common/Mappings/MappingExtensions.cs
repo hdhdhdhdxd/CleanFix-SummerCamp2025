ceb039af549799f39f3599eb6bb8ba19db1381a7
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-08-13 12:53:17 +0200

    refactor: reorganizar directivas using y agregar método ProjectToListAsync para mejorar la proyección de datos

--- CleanFix/Application/Common/Mappings/MappingExtensions.cs
+++ CleanFix/Application/Common/Mappings/MappingExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Infrastructure.Common.Models;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Infrastructure.Common.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Common.Mappings;
@@ -6,4 +8,7 @@ public static class MappingExtensions
 {
     public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
         => PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
+
+    public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration) where TDestination : class
+        => queryable.ProjectTo<TDestination>(configuration).AsNoTracking().ToListAsync();
 }
