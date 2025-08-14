e0266ceef1bf073a29f2bb3f2adbdf20b947cc7b
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-08-14 13:40:12 +0200

    feat: agregar configuración de entidad para Company y actualizar el contexto de la base de datos

---
+++ CleanFix/Infrastructure/Data/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+{
+    public void Configure(EntityTypeBuilder<Company> builder)
+    {
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(200)
+            .HasComment("Nombre de la empresa");
+    }
+}
