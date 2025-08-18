3e0239f45ee570a45dfa9166b09a13e028216285
Alvaro Gutierre <lanchoalvaro2001@gmail.com>
2025-08-18 12:52:39 +0200

    refactor: change CompanyId and ApartmentId properties to use int instead of Guid in CreateSolicitationDto

--- CleanFix/Application/Solicitations/Commands/CreateSolicitation/CreateSolicitationDto.cs
+++ CleanFix/Application/Solicitations/Commands/CreateSolicitation/CreateSolicitationDto.cs
@@ -8,9 +8,9 @@ public class CreateSolicitationDto
     [StringLength(200, MinimumLength = 5, ErrorMessage = "La descripción debe tener entre 5 y 200 caracteres.")]
     public string Description { get; set; }
     [Required(ErrorMessage = "El CompanyId es obligatorio.")]
-    public Guid CompanyId { get; set; }
+    public int CompanyId { get; set; }
     [Required(ErrorMessage = "El ApartmentId es obligatorio.")]
-    public Guid ApartmentId { get; set; }
+    public int ApartmentId { get; set; }
     private class Mapping : Profile
     {
         public Mapping()
