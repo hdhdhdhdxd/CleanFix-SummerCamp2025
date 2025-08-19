b4e6085a2bda445b4f4ea68f57c1f61efa8941ad
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-08-19 20:55:52 +0200

    Añadir clase UnitOfWork e implementar IUnitOfWork
    Se ha creado la clase `UnitOfWork` en el espacio de nombres `Infrastructure.Data`, que implementa la interfaz `IUnitOfWork`. Se ha añadido un campo privado `_database` de tipo `IDatabaseContext` y un constructor para inicializarlo. También se ha implementado el método `SaveChangesAsync` para guardar cambios de forma asíncrona. Se han añadido las declaraciones de uso de los espacios de nombres necesarios.

---
+++ CleanFix/Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Common.Interfaces;
+
+namespace Infrastructure.Data;
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly IDatabaseContext _database;
+
+    public UnitOfWork(IDatabaseContext database)
+    {
+        _database = database;
+    }
+
+    public async Task SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        await _database.SaveChangesAsync(cancellationToken);
+    }
+}
