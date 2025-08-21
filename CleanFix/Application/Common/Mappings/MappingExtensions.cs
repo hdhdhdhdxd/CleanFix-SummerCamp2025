11c94390f50f6896579aaa976f1a6f54db43e568
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-08-21 09:39:21 +0200

    refactor: Elimina el uso de AsNoTracking en las funciones de paginación y proyección

--- CleanFix/Application/Common/Mappings/MappingExtensions.cs
+++ CleanFix/Application/Common/Mappings/MappingExtensions.cs
@@ -7,8 +7,8 @@ namespace Infrastructure.Common.Mappings;
 public static class MappingExtensions
 {
     public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
-        => PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
+        => PaginatedList<TDestination>.CreateAsync(queryable, pageNumber, pageSize);
 
     public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration) where TDestination : class
-        => queryable.ProjectTo<TDestination>(configuration).AsNoTracking().ToListAsync();
+        => queryable.ProjectTo<TDestination>(configuration).ToListAsync();
 }
