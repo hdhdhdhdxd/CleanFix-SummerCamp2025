dbb9fb3bda1fb34dbb4c4019bf18b1decbf72f78
Alvaro Gutierre <lanchoalvaro2001@gmail.com>
2025-08-28 10:04:47 +0200

    feat: implement login form with authentication logic

--- CleanFix-frontend/src/app/ui/pages/login/login.ts
+++ CleanFix-frontend/src/app/ui/pages/login/login.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
 })
-export class Login {}
+export class Login {
+  username = ''
+  password = ''
+
+  onSubmit() {
+    // Aquí iría la lógica de autenticación
+    console.log('Usuario:', this.username)
+    console.log('Contraseña:', this.password)
+  }
+}
