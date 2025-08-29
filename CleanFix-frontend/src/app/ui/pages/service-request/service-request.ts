96c3d42e9d2cb8fde259363d6467bfb04ba57ace
AritzSanz <aritz.s@hotmail.es>
2025-08-29 10:15:22 +0200

    feat: Reemplazar componente de chatbox por chat y actualizar estilos

--- CleanFix-frontend/src/app/ui/pages/service-request/service-request.ts
+++ CleanFix-frontend/src/app/ui/pages/service-request/service-request.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
 import { ChatboxComponent } from '../chatbox/chatbox'
+import { ChatComponent } from './components/chat-component/chat.component'
 
 @Component({
   selector: 'app-service-request',
-  imports: [ChatboxComponent],
+  imports: [ChatboxComponent, ChatComponent],
   templateUrl: './service-request.html',
 })
 export class ServiceRequest {}
