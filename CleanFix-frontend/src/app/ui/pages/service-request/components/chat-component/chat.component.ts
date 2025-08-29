6d1f15ba875b1e6eea4991335c930c1eb7b3fe23
AritzSanz <aritz.s@hotmail.es>
2025-08-29 10:08:23 +0200

    feat: Agregar componente de chat con servicio de mensajería y pruebas

---
+++ CleanFix-frontend/src/app/ui/pages/service-request/components/chat-component/chat.component.ts
@@ -0,0 +1,34 @@
+// chat.component.ts
+import { Component, inject } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { ChatService } from 'src/app/ui/services/chat/chat.service'
+
+@Component({
+  selector: 'app-chat',
+  imports: [FormsModule],
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css'],
+})
+export class ChatComponent {
+  messages: { from: 'user' | 'bot'; text: string }[] = []
+  newMessage = ''
+
+  chatService = inject(ChatService)
+
+  sendMessage() {
+    const userMessage = this.newMessage.trim()
+    if (!userMessage) return
+
+    this.messages.push({ from: 'user', text: userMessage })
+    this.newMessage = ''
+
+    this.chatService.sendMessage(userMessage).subscribe({
+      next: (res) => {
+        this.messages.push({ from: 'bot', text: res.data })
+      },
+      error: () => {
+        this.messages.push({ from: 'bot', text: 'Ocurrió un error al procesar tu mensaje.' })
+      },
+    })
+  }
+}
