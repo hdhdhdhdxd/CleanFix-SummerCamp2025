de58f2eeaa005b47ea3a1e0cd453b88d100fd43a
Alvaro Gutierre <lanchoalvaro2001@gmail.com>
2025-08-29 13:08:18 +0200

    feat: update chat component layout and footer visibility logic

--- CleanFix-frontend/src/app/ui/main/main.ts
+++ CleanFix-frontend/src/app/ui/main/main.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { Component, inject } from '@angular/core'
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router'
 import { Footer } from '../shared/footer/footer'
 import { Header } from '../shared/header/header'
 import { Snackbar } from '../shared/snackbar/snackbar'
@@ -9,4 +9,17 @@ import { Snackbar } from '../shared/snackbar/snackbar'
   imports: [RouterOutlet, Footer, Header, Snackbar],
   templateUrl: './main.html',
 })
-export class Main {}
+export class Main {
+  showFooter = true
+
+  private router = inject(Router)
+
+  constructor() {
+    this.router.events.subscribe((event: unknown) => {
+      if (event instanceof NavigationEnd) {
+        // Oculta el footer en la ruta /service-request
+        this.showFooter = !event.urlAfterRedirects.startsWith('/service-request')
+      }
+    })
+  }
+}
