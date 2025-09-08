42df757da5495d4f69d2f6fda5c3ecb9041d56f7
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-09-09 01:20:14 +0200

    feat: actualizar enlaces a routerLink en componentes de la aplicación

--- CleanFix-frontend/src/app/ui/shared/header/header.ts
+++ CleanFix-frontend/src/app/ui/shared/header/header.ts
@@ -1,9 +1,10 @@
 import { Component, HostListener, inject, signal } from '@angular/core'
 import { Router } from '@angular/router'
+import { RouterLink } from '@angular/router'
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
 })
 export class Header {
