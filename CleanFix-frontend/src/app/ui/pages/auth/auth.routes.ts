82ad378b0fada2f30a281915fca6c47f014c96aa
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-09-09 02:20:38 +0200

    feat: agregar imagen de inicio de sesión y mejorar diseño de la página de autenticación

--- CleanFix-frontend/src/app/ui/pages/auth/auth.routes.ts
+++ CleanFix-frontend/src/app/ui/pages/auth/auth.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router'
 import { Login } from './components/login/login'
+import { Auth } from './auth'
 
 export const AUTH_ROUTES: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: Login },
   {
-    path: '**',
-    redirectTo: 'login',
+    path: '',
+    component: Auth,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: Login },
+      {
+        path: '**',
+        redirectTo: 'login',
+      },
+    ],
   },
 ]
